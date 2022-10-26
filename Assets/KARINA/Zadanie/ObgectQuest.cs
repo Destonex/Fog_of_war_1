2c3f61a4fcefe569eaae3904b9fc6d68025e31e7
Destonex <103641839+Destonex@users.noreply.github.com>
2022-10-26 22:14:41 +0300

    v0.2.6.16

--- Assets/KARINA/Zadanie/ObgectQuest.cs
+++ Assets/KARINA/Zadanie/ObgectQuest.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class ObgectQuest : MonoBehaviour
 {
     public Event_Play Qevent;
+    public GameObject mosin;
+    private Dialog_Next Text1;
+    //private NPC_Task PressStart;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mosin.SetActive(false);
     }
 
     // Update is called once per frame
@@ -16,12 +19,21 @@ public class ObgectQuest : MonoBehaviour
     {
         
     }
+
     void OnTriggerEnter(Collider col)
     {
-        if (col.tag == "Player")
-        {
-            Qevent.end_Quest1 = true;
-            Destroy(gameObject);
+        if (col.tag == "Player" && Text1 == true){
+            //NPC_Task.PressStart.SetActive(true);
+            if(Input.GetKeyDown("e")){
+               // NPC_Task.PressStart.SetActive(false);
+                Qevent.end_Quest1 = true;
+                mosin.SetActive(true);
+                Destroy(gameObject);
+            }
         }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        //NPC_Task.PressStart.SetActive(false);
     }
 }
